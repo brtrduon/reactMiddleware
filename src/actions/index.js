cdd36ceab97eda7411bb612a741277e735fd8d52
unknown <you@example.com>
2018-02-20 14:47:43 -0800

    added json for react to pull from but it's not get working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
+import axios from 'axios';
 import { FETCH_USERS } from './types';
 
 export function fetchUsers() {
+    const request = axios.get('http://jsonplaceholder.typicode.com/users');
+    // the link above is a pre-loaded list of users in AJAX / JSON format that we will use instead of pre-loading the page with dummy users
+
     return {
         type: FETCH_USERS,
-        payload: [
-            { name: 'Jane' },
-            { name: 'Bob' },
-            { name: 'Ada' }
-        ]
+        payload: request
     };
 }
